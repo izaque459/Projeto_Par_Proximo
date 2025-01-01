a1c6c6f25ee394df29d20a5c984cbe4a6ae4b5a9
Isaac_Santos <isaacdsantos@hotmail.com>
2025-01-01 20:04:48 -0300

    Modificado arquivo ParProximo com structs adicionados

--- ParProximo.c
+++ ParProximo.c
@@ -1,6 +1,22 @@
 
-struct Point2D
+typedef struct Point2D
 {
 	int x;
 	int y;
+}Point;
+
+typedef struct Pair2Points
+{
+	Point p1;
+	Point p2;
+}Pair;
+
+Pair ClosestPair(P [],int n)
+{
+	if (n<= 3)
+	{
+		
+	}
+		
+	
 }
